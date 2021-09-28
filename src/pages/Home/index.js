4547b569864b383e324ec62d31e92f6546db8e1f
Lucas Wedson <lwedson12@gmail.com>
2021-09-28 00:29:23 -0300

    Primeiro dia de desenvolvimento do Insider 4.0

---
+++ src/pages/Home/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+import { Container, SearchContainer, Input, SearchButton} from './styles';
+
+import {Feather} from '@expo/vector-icons'
+import Header from '../../components/Header';
+
+
+function Home(){
+    return(
+        <Container>
+            <Header title="React Prime"/>
+            <SearchContainer>
+                <Input
+                 placeholder="Ex Vingadores"
+                 placeholderTextColor="#ddd"
+                 />
+                <SearchButton>
+                    <Feather nome="Search" size={30} color="#FFF"/>
+                </SearchButton>
+
+            </SearchContainer>
+            
+
+        </Container>
+    )
+};
+
+export default Home;
