fac63a0747df61d70943e5cc9168961af93708cd
Lucas Wedson <lwedson12@gmail.com>
2021-09-30 21:46:48 -0300

    Terceiro dia de workshop

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,10 +1,18 @@
+
 import React, {useState, useEffect} from 'react';
 import {
     Container,
     Header,
     HeaderButton,
-    Banner
+    Banner,
+    ButtonLink,
+    Title,
+    ContentArea,
+    Rate,
+    ListGenres,
+    Description
 } from './styles';
+import { ScrollView, Modal } from 'react-native';
 
 import { Feather, Ionicons} from '@expo/vector-icons';
 
@@ -12,11 +20,16 @@ import { useNavigation, useRoute} from '@react-navigation/native';
 
 import api, {key} from '../../services/api';
 
+import Stars from 'react-native-stars';
+import Genres from '../../components/Genres';
+import ModalLink from '../../components/ModalLink';
+
 function Detail(){
     const navigation = useNavigation();
     const route = useRoute();
 
     const [movie, setMovie] = useState({});
+    const [openLink, setOpenLink] = useState(false);
 
     useEffect(() =>{
         let isActive = true;
@@ -33,11 +46,9 @@ function Detail(){
             })
             if(isActive){
                 setMovie(response.data);
-                console.log(response.data);
+                // console.log(response.data);
             }
         }
-
-
         if(isActive){
 
             getMovie();
@@ -54,7 +65,10 @@ function Detail(){
         <Container>
 
             <Header>
-                <HeaderButton>
+                <HeaderButton 
+                    activeOpacity={0.7} 
+                    onPress={()=> 
+                    navigation.goBack()}>
                     <Feather
                         name="arrow-left"
                         size={28}
@@ -70,6 +84,54 @@ function Detail(){
                     />
                 </HeaderButton>
             </Header>
+            <Banner
+                resizeMethod="resize"
+                source={{ uri: `https://image.tmdb.org/t/p/original/${movie.poster_path}`}}
+                        
+            />
+            <ButtonLink onPress={()=> setOpenLink(true)}>
+                <Feather name="link" size={24} color="#FFF"/>
+
+            </ButtonLink>
+            <Title numberOfLines={2}>{movie.title}</Title>
+
+            <ContentArea>
+                <Stars 
+                    default={movie.vote_average}
+                    count={10}
+                    half={true}
+                    starSize={20}
+                    fullStar={<Ionicons name="md-star" size={24} color="#E7A74e"/>}
+                    emptyStar={<Ionicons name="md-star-outline" size={24} color="#E7A74e"/>}
+                    halfStar={<Ionicons name="md-star-half" size={24} color="#E7A74e"/>}
+                    disable={true}    
+                />
+                <Rate>{movie.vote_average}/10 </Rate>
+            </ContentArea>
+
+            <ListGenres
+                data={movie?.genres}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(item)=> String(item.id)}
+                renderItem={({item})=> <Genres data={item}/>}
+            />  
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Title>Descrição</Title>
+                <Description>
+                    {movie?.overview}
+                </Description>
+            </ScrollView>
+
+            <Modal animationType="slide" transparent={true} visible={openLink}>
+                <ModalLink
+                    link={movie?.homepage}
+                    title={movie?.title}
+                    closeModal={()=> setOpenLink(false) }
+                />
+            </Modal>
+
 
         </Container>
     )
